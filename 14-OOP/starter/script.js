2063b8f0f38b8627f33a05021fe9c1f2869cf764
unknown <ival.dandong@gmail.com>
2025-04-21 21:07:04 +0700

    check

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Construcor Function and the new Operator
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calAge = function () {....}
+};
+
+const jonas = new Person('jonas', 1991);
